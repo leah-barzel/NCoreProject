10dc601c8ee0690e508969e13b64c67779ae7ecf
Malka Bruk <malka2039@gmail.com>
2023-11-22 22:50:55 +0200

    first commit

---
+++ Solid.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Solid.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        List<User> GetUsers();
+
+        User GetById(int id);
+
+        User AddUser(User user);
+
+        User UpdateUser(int id, User user);
+
+        void DeleteUser(int id);
+    }
+}
